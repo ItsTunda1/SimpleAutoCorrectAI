d02d172e12e4aad7025ee64e70743e665fb5d1a0
ItsTunda1 <97809650+ItsTunda1@users.noreply.github.com>
2024-03-04 17:15:33 -0500

    Store my changes

--- SimpleAutoCorrectAI/Source.cpp
+++ SimpleAutoCorrectAI/Source.cpp
@@ -10,16 +10,26 @@ typedef std::map<std::string, int> BasePairMap;
 
 int main()
 {
-    std::string text = "Hello Hi Bye";
-    const int wordCount = 2;
+    std::string text = "I like to talk I like to talk I like to talk I like to talk I like to talk";
+    const int wordCount = 7;
 
     BasePairMap dictionary;
-    int dataTable[wordCount][wordCount] = { {0} };
+    int dataTable1[wordCount][wordCount] = { {0} };
+    int dataTable2[wordCount][wordCount] = { {0} };
+    int dataTable3[wordCount][wordCount] = { {0} };
+    int dataTable4[wordCount][wordCount] = { {0} };
+    int dataTable5[wordCount][wordCount] = { {0} };
 
 
 
-    dictionary["Hello"] = 0;
-    dictionary["Hi"] = 1;
+    //This is 1 more than the real value stored
+    dictionary["Hello"] = 1;
+    dictionary["Hi"] = 2;
+    dictionary["I"] = 3;
+    dictionary["like"] = 4;
+    dictionary["to"] = 5;
+    dictionary["talk"] = 6;
+    dictionary["Bye"] = 7;
 
     std::cout << "Dictionary:" << std::endl;
     std::cout << "Hello:\t" << dictionary["Hello"] << std::endl;
@@ -31,14 +41,17 @@ int main()
 
     std::string word = "";
     std::vector<std::string> words;
+    std::cout << "\nText: " << text << std::endl;
     for (int i = 0; i < text.length(); i++)
     {
-        word += text[i];
         if (text[i] == ' ')
         {
             words.push_back(word);
             word = "";
+            continue;
         }
+
+        word += text[i];
 
     }
     words.push_back(word);
@@ -47,18 +60,128 @@ int main()
 
     //Print the words
     std::cout << "\n\n\nParsed Words:\n";
-    for (auto item : words)
+    /*for (auto item : words)
     {
         std::cout << item << std::endl;
-    }
+    }*/
 
 
 
     //Convert to Dictionary Values
-    int wordsEncoded[words.size()];
-    for (int i = 0; i < words.size(); i++)
+    std::vector<int> wordsEncoded;
+    for (int I = 0; I < words.size(); I++)
     {
-        wordsEncoded[i] = dictionary[words[i]];
+        wordsEncoded.push_back(dictionary[words[I]] - 1);
+        std::cout << words[I] << ":\t" << wordsEncoded[I] << std::endl;
+    }
+
+
+
+
+
+    //Create Data Set 1
+    for (int i = 0; i < wordsEncoded.size() - 1; i++)
+    {
+        if (wordsEncoded[i] > -1)
+            dataTable1[wordsEncoded[i]][wordsEncoded[i + 1]]++;
+    }
+
+    std::cout << "\nData Table 1:\n";
+    for (size_t x = 0; x < 7; x++)
+    {
+        for (size_t y = 0; y < 7; y++)
+        {
+            std::cout << dataTable1[x][y];
+        }
+        std::cout << "\n";
+    }
+
+
+
+    //Create Data Set 2
+    if (wordsEncoded.size() >= 2)
+    {
+        for (int i = 0; i < wordsEncoded.size() - 2; i++)
+        {
+            if (wordsEncoded[i] > -1)
+                dataTable2[wordsEncoded[i]][wordsEncoded[i + 2]]++;
+        }
+    }
+
+    std::cout << "\nData Table 2:\n";
+    for (size_t x = 0; x < 7; x++)
+    {
+        for (size_t y = 0; y < 7; y++)
+        {
+            std::cout << dataTable2[x][y];
+        }
+        std::cout << "\n";
+    }
+
+
+
+    //Create Data Set 3
+    if (wordsEncoded.size() >= 3)
+    {
+        for (int i = 0; i < wordsEncoded.size() - 3; i++)
+        {
+            if (wordsEncoded[i] > -1)
+                dataTable3[wordsEncoded[i]][wordsEncoded[i + 3]]++;
+        }
+    }
+
+    std::cout << "\nData Table 3:\n";
+    for (size_t x = 0; x < 7; x++)
+    {
+        for (size_t y = 0; y < 7; y++)
+        {
+            std::cout << dataTable3[x][y];
+        }
+        std::cout << "\n";
+    }
+
+
+
+    //Create Data Set 4
+    if (wordsEncoded.size() >= 4)
+    {
+        for (int i = 0; i < wordsEncoded.size() - 4; i++)
+        {
+            if (wordsEncoded[i] > -1)
+                dataTable4[wordsEncoded[i]][wordsEncoded[i + 4]]++;
+        }
+    }
+
+    std::cout << "\nData Table 4:\n";
+    for (size_t x = 0; x < 7; x++)
+    {
+        for (size_t y = 0; y < 7; y++)
+        {
+            std::cout << dataTable4[x][y];
+        }
+        std::cout << "\n";
+    }
+
+
+
+    //Create Data Set 5
+    if (wordsEncoded.size() >= 5)
+    {
+        for (int i = 0; i < wordsEncoded.size() - 5; i++)
+        {
+            if (wordsEncoded[i] > -1)
+                dataTable5[wordsEncoded[i]][wordsEncoded[i + 5]]++;
+        }
+    }
+
+    std::cout << "\nData Table 5:\n";
+    for (size_t x = 0; x < 7; x++)
+    {
+        for (size_t y = 0; y < 7; y++)
+        {
+            std::cout << dataTable5[x][y];
+        }
+        std::cout << "\n";
     }
 
 
