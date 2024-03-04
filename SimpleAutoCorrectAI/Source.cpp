dcae207398c03e3f14bb6ef273fb07a6e5271571
ItsTunda1 <97809650+ItsTunda1@users.noreply.github.com>
2024-03-03 23:30:07 -0500

    Add project files.

---
+++ SimpleAutoCorrectAI/Source.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <map>
+using namespace std;
+
+typedef std::map<string, int> BasePairMap;
+
+
+
+
+int main()
+{
+    
+    BasePairMap dictionary;
+    m["Hello"] = 0;
+    m["Hi"] = 1;
+
+    std::cout << "Hello:\t" << dictionary["Hello"] << std::endl;
+    std::cout << "Hi:\t" << dictionary["Hi"] << std::endl;
+
+
+
+
+
+
+	std::cout << "\n\n\n\n\n";
+	return 0;
+}
