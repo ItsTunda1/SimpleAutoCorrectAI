293021965a66b84a37997ac42ec8903ad96952d9
ItsTunda1 <97809650+ItsTunda1@users.noreply.github.com>
2024-03-04 11:17:09 -0500

    adding counting

--- SimpleAutoCorrectAI/Source.cpp
+++ SimpleAutoCorrectAI/Source.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <map>
+#include <list>
+#include <string>
 using namespace std;
 
 typedef std::map<string, int> BasePairMap;
@@ -10,10 +12,13 @@ typedef std::map<string, int> BasePairMap;
 int main()
 {
     string text = "Hello Hi Bye";
+    const int wordCount = 2;
 
-
-    
     BasePairMap dictionary;
+    int dataTable[wordCount][wordCount] = { {0} };
+
+
+
     dictionary["Hello"] = 0;
     dictionary["Hi"] = 1;
 
@@ -25,19 +30,37 @@ int main()
 
 
 
-    std::cout << "\n\n\nParsed Words:\n";
     string word = "";
+    list<string> words;
     for (int i = 0; i < text.length(); i++)
     {
         word += text[i];
         if (text[i] == ' ')
         {
-            std::cout << word << std::endl;
+            words.push_back(word);
             word = "";
         }
 
     }
-    std::cout << word << std::endl;
+    words.push_back(word);
+
+
+
+    //Print the words
+    std::cout << "\n\n\nParsed Words:\n";
+    for (auto item : words)
+    {
+        std::cout << item << std::endl;
+    }
+
+
+
+    //Convert to Dictionary Values
+    /*int wordsEncoded[words.size()];
+    for (int i = 0; i < words.size(); i++)
+    {
+        wordsEncoded[i] = dictionary[words[i]];
+    }*/
 
 
 
