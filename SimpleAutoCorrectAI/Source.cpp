6c479bf05ea6f4578266b39b80e27ca86024dab6
ItsTunda1 <97809650+ItsTunda1@users.noreply.github.com>
2024-03-04 10:23:42 -0500

    Dictionary and Parsing

--- SimpleAutoCorrectAI/Source.cpp
+++ SimpleAutoCorrectAI/Source.cpp
@@ -9,13 +9,35 @@ typedef std::map<string, int> BasePairMap;
 
 int main()
 {
+    string text = "Hello Hi Bye";
+
+
     
     BasePairMap dictionary;
-    m["Hello"] = 0;
-    m["Hi"] = 1;
+    dictionary["Hello"] = 0;
+    dictionary["Hi"] = 1;
 
+    std::cout << "Dictionary:" << std::endl;
     std::cout << "Hello:\t" << dictionary["Hello"] << std::endl;
     std::cout << "Hi:\t" << dictionary["Hi"] << std::endl;
+
+
+
+
+
+    std::cout << "\n\n\nParsed Words:\n";
+    string word = "";
+    for (int i = 0; i < text.length(); i++)
+    {
+        word += text[i];
+        if (text[i] == ' ')
+        {
+            std::cout << word << std::endl;
+            word = "";
+        }
+
+    }
+    std::cout << word << std::endl;
 
 
 
